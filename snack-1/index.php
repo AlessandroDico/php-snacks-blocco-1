ddb22e4ec71c8dad99e3ac2e52e4e8b73f300698
Ale <alessandrochb@gmail.com>
2020-12-15 17:26:13 +0100

    aggiunta una seconda soluzione (commentata) allo snack-1

--- snack-1/index.php
+++ snack-1/index.php
@@ -119,3 +119,101 @@ for ($i = 0; $i < count($matches); $i++) {
         </p>
     </body>
 </html>
+<?php
+
+//SECONDA SOLUZIONE
+/*
+$matches = [
+    [
+        "casa" => [
+            "team" => "Olimpia Milano",
+            "punti" => 60
+        ],
+        "ospiti" => [
+            "team" => "Cantù",
+            "punti" => 40
+        ],
+    ],
+    [
+        "casa" => [
+            "team" => "Varese",
+            "punti" => 50
+        ],
+        "ospiti" => [
+            "team" => "Cantù",
+            "punti" => 45
+        ],
+    ],
+    [
+        "casa" => [
+            "team" => "Varese",
+            "punti" => 50
+        ],
+        "ospiti" => [
+            "team" => "Virtus Bologna",
+            "punti" => 35
+        ],
+    ],
+    [
+        "casa" => [
+            "team" => "Victoria Libertas",
+            "punti" => 50
+        ],
+        "ospiti" => [
+            "team" => "Virtus Bologna",
+            "punti" => 45
+        ],
+    ],
+    [
+        "casa" => [
+            "team" => "Allianz Trieste",
+            "punti" => 50
+        ],
+        "ospiti" => [
+            "team" => "Brescia",
+            "punti" => 55
+        ],
+    ],
+    [
+        "casa" => [
+            "team" => "Venezia Reyer",
+            "punti" => 50
+        ],
+        "ospiti" => [
+            "team" => "Olimpia Milano",
+            "punti" => 60
+        ],
+    ],
+];
+
+
+$nuovoArray = [];
+for ($i = 0; $i < count($matches); $i++) {
+
+    $nuovoArray[] = $matches[$i]["casa"]["team"] . ' - ' . $matches[$i]["ospiti"]["team"] . ' | '. $matches[$i]["casa"]["punti"] . ' - ' . $matches[$i]["ospiti"]["punti"];
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Snack-1-Bis</title>
+    </head>
+    <body>
+        <h1>
+            Partite torneo 2020
+        </h1>
+        <span style="margin-right:150px;font-size:35px;">Squadre</span> <span style="font-size:35px;">Risultati</span>
+        <p style="font-size:25px;">
+            <?php
+            for ($i = 0; $i < count($nuovoArray); $i++) {
+
+                echo "<h2 style=\"color:green;\">" . $nuovoArray[$i] . "</h2>";
+
+            }
+            ?>
+        </p>
+    </body>
+</html>
+*/
